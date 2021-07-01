450c63e83dc7e0dafcfcc161d9fd238c707215e3
ChrisJ2005 <chrisjaison05@outlook.com>
2021-07-01 14:50:11 -0400

    added code for 2d gen

--- mytest.py
+++ mytest.py
@@ -1,2 +1,15 @@
-print("this is another test")
-print("added new line")
+x = input("Enter Score between 0.0 and 1.0: ")
+number = float(x)
+if number >= 0.9 :
+    print('A')
+elif number >= 0.8 :
+    print ('B')
+elif number >= 0.7 :
+    print ('C')
+elif number >= 0.6 :
+    print ('D')
+elif number < 0.6 :
+    print ('F')
+else :
+    print('Error, incorrect grade has been put in')
+    #Second Program Ever Done
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-This is a test doc
+#Free space to practice coding
